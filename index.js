a0776b407de74aa75d3a773822f7f4df1c99b25b
Innovation Hub <innovation.hub@innovationhub.ug>
2026-03-25 12:25:44 +0300

    more of java script

---
+++ index.js
@@ -0,0 +1,56 @@
+// // else- if statement
+// //definition of else if statement
+// if (condition) {
+//     // code to be executed if condition is true
+// } else if (condition2) {
+//     // code to be executed if condition2 is true
+// } else { 
+//     // code to be executed if both condition1 and conditional2 are false
+// } else {
+//     // code to be executed if all conditions are false
+// }
+
+
+// practice examples
+// let mark = 50
+
+// if (mark > 69 ) {
+//     console.log("very good, you have passed")
+// }
+
+// if (mark >= 70 ){
+//     console.log("You have failed the exam")
+// }
+//Comparison Operators
+// 1. Equal to (==)
+// 2. Not equal to (!=)
+// 3. Stricr equal to (===)
+// 4. Strict not equal to (!==)
+// 5. Greater than (>)
+// 6. Less than(<)
+// 7. Greater than or equal to( >=)
+// 8. less than or equal to (<=)
+
+// Arrays
+let cars = ["BMW", "Benz", "jeep"];
+console.log(cars);
+console.log(cars[2]);
+ 
+let mixedArray = ["kent", 20, "uganda",true, null, undefined, { name: "martha" } ];
+console.log(mixedArray);
+console.log(mixedArray[6])
+
+let person= {
+    name: "martha",
+    age: 20,
+    district: "kampala",
+
+
+
+
+
+
+
+
+}
+
